f3e0624ccbd2cac7dfe7742ae8eb11bc1554ebb4
snjy-01 <103566986+snjy-01@users.noreply.github.com>
2022-04-13 15:09:38 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+       <h1>Institute: Maulana Azad National Institute of Technology, Bhopal</h1>
+       <h2>Developed by: Sanjay Dhakad</h2>
+       <h2>Scholar No.: 212120101</h2>
+       <h2>Lab 208 project</h2>
+    </div>
+  )
+}
